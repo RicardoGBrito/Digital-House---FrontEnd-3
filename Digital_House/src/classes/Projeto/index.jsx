1a98f2fb2ed171cf05b9e30574244a6f733412e2
RicardoGBrito <Ricardobrito_348@hotmail.com>
2022-10-24 18:33:36 -0300

    Correção: a pasta projeto estava com o nome diferente da função

--- Digital_House/src/classes/Projeto/index.jsx
+++ Digital_House/src/classes/Projeto/index.jsx
@@ -1,6 +1,6 @@
 import './style.scss';
 
-export function SegundaAula(){
+export function Projeto(){
 
     /* const titulos = [
         {titulo:'Assista ao vivo'},
@@ -9,9 +9,9 @@ export function SegundaAula(){
 ] */
 
     const conteudo = [
-        {titulo:'Assista ao vivo', /* imagem:'https://globoplay.globo.com/tv-globo/ao-vivo/6120663/',  */tituloCard:'Card Canal', descricao1:'Hora Card', descricao2:'Conteúdo Card'},
-        {titulo:'Todas as Flores - Trechos gratuitos', /* imagem:'https://globoplay.globo.com/v/11047435/editorial/a67b3ece-9f82-4020-9183-13c49ad7450f/', */ tituloCard:'Todas as Flores', descricao1:'Vanessa descobre que Maira e Raael estão juntos', descricao2:''},
-        {titulo:'Novidades',/* imagem:'https://globoplay.globo.com/gonjiam-manicomio-assombrado/t/KgxfyQsJsb/', */ tituloCard:'', descricao1:'', descricao2:''},
+        {titulo:'Assista ao vivo', imagem:'https://globoplay.globo.com/tv-globo/ao-vivo/6120663/', tituloCard:'Card Canal', descricao1:'Hora Card', descricao2:'Conteúdo Card'},
+        {titulo:'Todas as Flores - Trechos gratuitos', imagem:'https://globoplay.globo.com/v/11047435/editorial/a67b3ece-9f82-4020-9183-13c49ad7450f/', tituloCard:'Todas as Flores', descricao1:'Vanessa descobre que Maira e Raael estão juntos', descricao2:''},
+        {titulo:'Novidades',imagem:'https://globoplay.globo.com/gonjiam-manicomio-assombrado/t/KgxfyQsJsb/', tituloCard:'', descricao1:'', descricao2:''},
 ]
 
     return (
@@ -28,19 +28,20 @@ export function SegundaAula(){
                 <li>
                     <h2>Carrossel de Divulgação</h2>
                 </li>
-                {conteudo.map((objLista) => (
+                {
+                    conteudo.map((objLista) => (
 
-                    <li>
-                        <h2>{objLista.titulo}</h2>
                         <li>
-                            {/* <img src={objLista.imagem}/> */}
-                            <h3>{objLista.tituloCard}</h3>
-                            <p>{objLista.descricao1}</p>
-                            <p>{objLista.descricao2}</p>
+                            <h2>{objLista.titulo}</h2>
+                            <li>
+                                <img src={objLista.imagem}/>
+                                <h3>{objLista.tituloCard}</h3>
+                                <p>{objLista.descricao1}</p>
+                                <p>{objLista.descricao2}</p>
+
+                            </li>
 
                         </li>
-
-                    </li>
 
 
                     ))
