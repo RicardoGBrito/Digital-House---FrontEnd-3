18672998ea0afd566f517318c1828a011033fbba
RicardoGBrito <Ricardobrito_348@hotmail.com>
2022-11-06 18:59:42 -0300

    Adição de um identificador único em cada componente

--- DH_Game/src/pages/PaginaPrincipal/index.jsx
+++ DH_Game/src/pages/PaginaPrincipal/index.jsx
@@ -10,6 +10,7 @@ export function PaginaPrincipal(){
 
     const newGames = [
         {
+            id:1,
             name: 'Hollow Knight',
             plataforms: ['windows'],
             categories: ['Metroidvania', 'Souls-like', 'Platformer2D'],
@@ -17,6 +18,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/367520/header.jpg'
         },
         {
+            id:2,
             name: "Don't Starve",
             plataforms: ['windows'],
             categories: ['SurvivalOpen', 'World Survival', 'CraftCrafting'],
@@ -24,6 +26,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/219740/header.jpg'
         },
         {
+            id:3,
             name: 'Cult of the Lamb',
             plataforms: ['windows'],
             categories: ['Horror', 'Cute', 'Roguelike'],
@@ -34,6 +37,7 @@ export function PaginaPrincipal(){
     
     const halloweenGames = [
         {
+            id:1,
             name: 'Inscryption',
             categories: ['Card', 'BattlerCard', 'GameStory Rich'],
             oldPrice: '100,00',
@@ -41,6 +45,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/1092790/header.jpg'
         },
         {
+            id:2,
             name: 'Hades',
             categories: ['Action', 'Roguelike', 'Indie', 'Action'],
             oldPrice: '80,00',
@@ -48,6 +53,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/1145360/header.jpg'
         },
         {
+            id:3,
             name: 'Darkest Dungeon',
             categories: ['Turn-BasedCombat', 'DarkFantasy', 'Roguelike'],
             oldPrice: '30,00',
@@ -55,6 +61,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/262060/header.jpg'
         },
         {
+            id:4,
             name: 'Outlast',
             categories: ['Horror', 'First-Person', 'Survival Horror'],
             oldPrice: '100,00',
@@ -65,6 +72,7 @@ export function PaginaPrincipal(){
     
     const earlyAccessGames = [
         {
+            id:1,
             name: 'Stardew Valleyt',
             plataforms: ['windows'],
             categories: ['Farming Sim', 'Life Sim', 'Pixel Graphics'],
@@ -72,6 +80,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/413150/header.jpg'
         },
         {
+            id:2,
             name: "Little Nightmares",
             plataforms: ['windows'],
             categories: ['Horror', 'Atmospheric', 'Adventure'],
@@ -79,6 +88,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/424840/header.jpg'
         },
         {
+            id:3,
             name: 'Bendy and the Ink Machine',
             plataforms: ['windows'],
             categories: ['Horror', 'First-Person', 'Singleplayer', 'PuzzleDark'],
@@ -89,6 +99,7 @@ export function PaginaPrincipal(){
     
     const cheapGames = [
         {
+            id:1,
             name: 'Little Misfortune',
             categories: ['Adventure', 'Point & Click', 'Indie'],
             oldPrice: '100,00',
@@ -96,6 +107,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/714120/header.jpg'
         },
         {
+            id:2,
             name: 'Limbo',
             categories: ['Puzzle', 'Platformer', 'Indie'],
             oldPrice: '80,00',
@@ -103,6 +115,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/48000/header.jpg'
         },
         {
+            id:3,
             name: 'Darkwood',
             categories: ['Horror', 'Survival'],
             oldPrice: '30,00',
@@ -110,6 +123,7 @@ export function PaginaPrincipal(){
             picture: 'https://cdn.akamai.steamstatic.com/steam/apps/274520/header.jpg'
         },
         {
+            id:4,
             name: 'Slender: The Arrival',
             categories: ['Horror', 'Survival Horror', 'First-Person'],
             oldPrice: '100,00',
@@ -153,7 +167,9 @@ export function PaginaPrincipal(){
                 <ul>
                     {
                         newGames.map(game => (
-                            <ComponenteCardRetangular gamesItens = {game}/>
+                            <ComponenteCardRetangular
+                            key={game.id} 
+                            gamesItens = {game}/>
                         ))
                     }
                 </ul>
@@ -163,7 +179,9 @@ export function PaginaPrincipal(){
                 <ul className="grid_list">
                     {
                         halloweenGames.map(hallGame => (
-                            <ComponenteCardQuadrado gamesItens = {hallGame}/>
+                            <ComponenteCardQuadrado
+                            key = {hallGame.id} 
+                            gamesItens = {hallGame}/>
                         ))
                     }
                 </ul>
@@ -173,7 +191,9 @@ export function PaginaPrincipal(){
                 <ul>
                     {
                         earlyAccessGames.map(accessGame => (
-                            <ComponenteCardRetangular gamesItens = {accessGame}/>
+                            <ComponenteCardRetangular 
+                            key={accessGame.id}
+                            gamesItens = {accessGame}/>
                         ))
                     }
                 </ul>
@@ -183,7 +203,9 @@ export function PaginaPrincipal(){
                 <ul className="grid_list">
                     {
                         cheapGames.map(cheapGame => (
-                            <ComponenteCardQuadrado gamesItens = {cheapGame}/>
+                            <ComponenteCardQuadrado 
+                            key = {cheapGame.id}
+                            gamesItens = {cheapGame}/>
                         ))
                     }
                 </ul>
